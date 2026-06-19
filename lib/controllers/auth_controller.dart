cd784568dd39b081a0be355da8aabf10a501078a
Mohib Khorajiya <mohibkhorajiya848@gmail.com>
2026-06-19 18:48:28 +0530

    Added new photos to the project

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import '../core/constants/app_colors.dart';
 
